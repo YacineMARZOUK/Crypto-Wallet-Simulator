2ea83a975a5ab215aa7b435251218cd09c3fe259
YacineMARZOUK <yacinemarzou@gmail.com>
2025-09-23 10:57:39 +0100

    lajoute d'ethereumFeeCalculator

---
+++ src/service/EthereumFeeCalculator.java
@@ -0,0 +1,32 @@
+package service;
+import java.math.BigDecimal;
+import com.crypto.model.FeePriority;
+import com.crypto.model.Transaction;
+
+public class EthereumFeeCalculator implements FeeCalculator {
+private static final BigDecimal ECONOMIQUE_GAS_PRICE = BigDecimal("10");
+private static final BigDecimal STANDARD_GAS_PRICE = BigDecimal("20");
+private static final BigDecimal FAST_GAS_PRICE = BigDecimal("40");
+private static final BigDecimal GAS_LIMIT = BigDecimal("21000");
+
+@Override
+    public BigDecimal calculateFees (Transaction tx){
+    BigDecimal gasPrice;
+    switch (tx.getPriority()){
+        case ECONOMIQUE:
+            gasPrice = ECONOMIQUE_GAS_PRICE;
+            break;
+        case STANDARD:
+            gasPrice = STANDARD_GAS_PRICE;
+            break;
+        case RAPIDE:
+            gasPrice =  FAST_GAS_PRICE;
+            break;
+        default:
+            gasPrice = STANDARD_GAS_PRICE;
+    }
+    BigDecimal totalGwei = GAS_LIMIT.multiply(gasPrice);
+    return totalGwei.divide(new BigDecimal("1000000000"), 9, BigDecimal.ROUND_HALF_UP);
+}
+
+}
